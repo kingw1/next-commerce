42a13a55efbbb29577421de36164b67c3f91836c
King <wiwichai@gmail.com>
2024-05-04 18:13:54 +0700

    change logo

--- components/backoffice/Sidebar.jsx
+++ components/backoffice/Sidebar.jsx
@@ -2,7 +2,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import React, { useState } from "react";
-import logo from "../../public/logo-dark.png";
+import logo from "../../public/next-shop-logo.png";
 import {
   ChevronDown,
   ChevronRight,
