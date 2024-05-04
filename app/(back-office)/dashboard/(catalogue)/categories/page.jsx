ca92ba38046346b8ca81325d98f8b7082b219c46
King <wiwichai@gmail.com>
2024-05-04 18:14:54 +0700

    update categories form

--- app/(back-office)/dashboard/(catalogue)/categories/page.jsx
+++ app/(back-office)/dashboard/(catalogue)/categories/page.jsx
@@ -1,9 +1,21 @@
+import PageHeader from "@/components/backoffice/PageHeader";
+import TableActions from "@/components/backoffice/TableActions";
 import React from "react";
 
 export default function page() {
   return (
     <div>
-      <h2>Categories</h2>
+      <PageHeader
+        heading="Categories"
+        linkTitle="Add Category"
+        href="/dashboard/categories/new"
+      />
+
+      <TableActions />
+
+      <div className="py-8">
+        <h2>Table</h2>
+      </div>
     </div>
   );
 }
