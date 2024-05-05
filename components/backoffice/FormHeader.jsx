983d9f04e44acbd6533a01c08925b3adae75aa9e
King <wiwichai@gmail.com>
2024-05-05 11:20:29 +0700

    add close form

--- components/backoffice/FormHeader.jsx
+++ components/backoffice/FormHeader.jsx
@@ -1,11 +1,13 @@
 import { X } from "lucide-react";
+import { useRouter } from "next/navigation";
 import React from "react";
 
 export default function FormHeader({ title }) {
+  const router = useRouter();
   return (
-    <div className="flex items-center justify-between py-6 px-12 bg-white shadow text-slate-800 dark:bg-slate-600 dark:text-slate-50 rounded-lg">
+    <div className="flex items-center justify-between py-6 px-12 bg-white shadow text-slate-800 dark:bg-slate-600 dark:text-slate-50 rounded-lg mb-12">
       <h2 className="text-xl font-semibold">{title}</h2>
-      <button className="">
+      <button className="" onClick={() => router.back()}>
         <X />
       </button>
     </div>
